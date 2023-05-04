db65ebe78871d8cffe121e6ae61f3cf6593ef44e
jimmy1020227 <lijingyan278@gmail.com>
2023-05-04 18:15:09 +0800

    init

---
+++ uno_soil_relay/uno_soil_relay.ino
@@ -0,0 +1,38 @@
+
+ int relayPin=10;
+  int ledPin=13;
+  int value=0;  
+void setup() {
+   
+  Serial.begin(9600);
+  Serial.println("jkhk");
+  pinMode(relayPin,OUTPUT);
+  pinMode(ledPin,OUTPUT);
+  delay(200);
+}
+
+void loop() {
+ value=analogRead(A0);
+ Serial.print("value");
+ Serial.println(value);
+ 
+ if(value>600)
+ {
+
+  digitalWrite(relayPin,HIGH);
+  digitalWrite(ledPin,HIGH);
+  delay(1500); 
+  
+   //digitalWrite(relayPin,LOW);
+  //digitalWrite(ledPin,LOW);
+
+ }
+ else
+ {
+  digitalWrite(relayPin,LOW);
+  digitalWrite(ledPin,LOW);
+
+ }
+
+ delay(200);
+}
