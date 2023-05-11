bb6d9947914d242f1762f76071f245063827242f
jimmy1020227 <132564144+jimmy1020227@users.noreply.github.com>
2023-05-11 18:38:05 +0800

    20230511

--- basic1/basic1.ino
+++ basic1/basic1.ino
@@ -1,31 +1,44 @@
-int a=10;
+
+int a =0;
 
 void setup() {
-  int a=10;
+  int a =1000;
   Serial.begin(9600);
   a = test(10);
   Serial.println(a);
-
 }
+
 int test(int x)
 {
-  int rtn=3;
-  return rtn *x;
+  int rtn =10;
+  
+  return rtn*x;
 }
+
 void loop() {
- int a=10;  
-a=test(10);
-Serial.println(a);
 
-a= test(20);
-Serial.println(a);
+  if(false)//true
+  {
+    Serial.println("-----------------if------------");
+  }
+  else
+  {
+    Serial.println("else");
+    
+  }
+  
 
-
-a=test(30);
-Serial.println(a);
-
-
-a=test(40);
-Serial.println(a);
-delay(1000);
+  a = test(10);
+  Serial.println(a);
+  
+  
+  a = test(20);
+  Serial.println(a);
+  
+  a = test(30);
+  Serial.println(a);
+  
+  a = test(40);
+  Serial.println(a);
+  delay(1000);
 }
